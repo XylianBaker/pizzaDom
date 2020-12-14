8a685bbd103b58d9f5b1d4ebad3838d3cde79443
Jan Kammellander <janschwiderik@gmail.com>
2020-12-14 02:17:56 +0100

    Person 🕴 and Pizza 🍕 Object definded.

---
+++ app.js
@@ -0,0 +1,15 @@
+const person = {
+    firstname: "",
+    lastname: "",
+    address: "",
+    postalCode: 0,
+    birthday: "",
+    gender: ""
+}
+
+const pizza = {
+    type: "",
+    hotness: 0,
+    extras: { "", "" },
+    additional: ""
+}
